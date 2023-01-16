5eb9e7e70c30dc8b17d28f36c1ba79f016a62bdb
ritgaug <98553806+ritgaug@users.noreply.github.com>
2023-01-16 09:23:01 -0500

    Update Lab2.java

--- src/lab2/Lab2.java
+++ src/lab2/Lab2.java
@@ -14,7 +14,7 @@ public class Lab2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World cloning");
     }
     
 }
